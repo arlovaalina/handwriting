d403aa26727649bbbe534ed5eaf2c5002385742b
arlovaalina <arlovaalina@gmail.com>
2018-05-13 15:17:17 +0300

    Add canvas and drawing on it

--- src/components/main/components/CanvasCard.jsx
+++ src/components/main/components/CanvasCard.jsx
@@ -8,13 +8,17 @@ class CanvasCard extends React.Component {
   }
 
   getOffsetSum = (elem) => {
-    var top=0, left=0
-    while(elem) {
-        top = top + parseInt(elem.offsetTop)
-        left = left + parseInt(elem.offsetLeft)
-        elem = elem.offsetParent
+    let top = 0;
+    let left = 0;
+    while (elem) {
+      top = top + parseInt(elem.offsetTop);
+      left = left + parseInt(elem.offsetLeft);
+      elem = elem.offsetParent;
     }
-    return {top: top, left: left};
+    return {
+      top,
+      left,
+    };
   }
 
   onMouseDown = (event) => {
@@ -22,28 +26,24 @@ class CanvasCard extends React.Component {
     const x = event.pageX - this.getOffsetSum(this.refs.canvas).left;
     const y = event.pageY - this.getOffsetSum(this.refs.canvas).top;
 
-    console.log(event.target.offsetTop);
-
-    console.log(x);
-    console.log(y);
-
     this.setState({
       ...this.state,
       context,
       startDrawing: true,
     });
     context.beginPath();
-    context.lineWidth = 10;
+    context.lineWidth = 5;
     context.lineCap="round";
+    context.strokeStyle="#585858";
     context.moveTo(x, y);
   }
 
   onMouseMove = (event) => {
-    if (this.state.startDrawing) {
+    const { context, startDrawing } = this.state;
+    if (startDrawing) {
       const x = event.pageX - this.getOffsetSum(this.refs.canvas).left;
       const y = event.pageY - this.getOffsetSum(this.refs.canvas).top;
-      const { context } = this.state;
-      console.log('move');
+
       context.lineTo(x, y);
       context.stroke();
     }
@@ -54,7 +54,6 @@ class CanvasCard extends React.Component {
       ...this.state,
       startDrawing: false,
     });
-    console.log('up');
   }
 
   render() {
@@ -67,12 +66,12 @@ class CanvasCard extends React.Component {
           Please, draw a digit from 0 to 9 in the field below.
           After you have drawn, click the "Recognize" button and compare results.
         </div>
-        <div className="card__main">
+        <div className="card__canvas">
           <canvas
             ref="canvas"
-            className="card__canvas"
-            name="canvas"
-            id="canvas"
+            className="canvas"
+            width="280"
+            height="280"
             onMouseDown={this.onMouseDown}
             onMouseMove={this.onMouseMove}
             onMouseUp={this.onMouseUp}
